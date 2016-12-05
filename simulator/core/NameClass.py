bb4f673c20022ee1a1451ea23566f62ebd29de02
lei.zhan <lei.zhan@schneider-electric.com>
2016-12-05 11:41:24 +0800

    Delete config.ini NameClass can be init as list or string

--- simulator/core/NameClass.py
+++ simulator/core/NameClass.py
@@ -2,6 +2,10 @@ class NameClass(object):
     def __init__(self, name, items):
         name = str(name)
         self.new_class = None
+        if type(items) is str:
+            items = items.split(',')
+            for i in range(len(items)):
+                items[i] = items[i].strip(' ')
         class_text_head = 'class ' + name + ':\n'
         class_text_body = '    __fields = []\n'
         for item in items:
@@ -9,13 +13,18 @@ class NameClass(object):
             class_text_body += '    ' + '__fields.append(\'' + str(item) + '\')\n'
         class_text_body += '    fields = tuple(__fields)\n'
         class_text_body += '    del __fields\n'
-        class_text_body += '''    def __init__(self, *args):
+        class_text_body += '''    def __init__(self, *args, **kwargs):
         i = 0
         for val in args:
             setattr(self, self.fields[i], val)
             if i + 1 >= len(self.fields):
                 break
             i += 1
+        for (key, val) in kwargs.items():
+            if key in self.fields:
+                setattr(self, key, val)
+            else:
+                pass
         pass
         \n'''
         class_text = class_text_head + class_text_body + 'self.new_class = ' + name
