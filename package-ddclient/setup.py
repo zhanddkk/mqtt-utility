1cc61d8b3509e70bcb3d3617048861cd8ed51af7
lei.zhan <lei.zhan@schneider-electric.com>
2017-01-05 17:06:01 +0800

    Fix the bug of setup script in ddclient package that the error may occur when run it

--- package-ddclient/setup.py
+++ package-ddclient/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
-from os import path
+from os import path, chdir
 
 here = path.abspath(path.dirname(__file__))
-
+chdir(here)
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
